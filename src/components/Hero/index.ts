a7d7dcaf8c2ba3205bef00752dc2eb68a51fafdc
Lav Kumar Yadav <lav.yadav@joshtechnologygroup.com>
2025-10-30 20:58:50 +0530

    LY_A2_03 : Hero : Added Hero Section, It's responsiveness and accessability

---
+++ src/components/Hero/index.ts
@@ -0,0 +1,2 @@
+export type { HeroConfigType } from './Hero.types';
+export { Hero } from './Hero.components';
--- src/layouts/RootLayout/RootLayout.layout.tsx
+++ src/layouts/RootLayout/RootLayout.layout.tsx
@@ -18,21 +18,23 @@ export const RootLayout = () => {
 
     return (
         <Grid container>
-            <Grid size={12} height={60}>
+            <Grid size={12} height={75}>
                 <Header setIsSidebarOpen={setIsSideBarOpen} />
             </Grid>
-            <Grid width={{ xs: 0, md: 300 }}>
-                <SideBar
-                    open={isSideBarOpen}
-                    sideBarConfig={sideBarConfig}
-                    sidebarBottomIcon={sidebarBottomIcon}
-                    aria-label="Sidebar"
-                    onClose={handleSidebarClose}
-                />
-            </Grid>
-            <Grid>
-                <Outlet />
-                <Footer />
+            <Grid container>
+                <Grid size={{ xs: 0, md: 'auto' }}>
+                    <SideBar
+                        open={isSideBarOpen}
+                        sideBarConfig={sideBarConfig}
+                        sidebarBottomIcon={sidebarBottomIcon}
+                        aria-label="Sidebar"
+                        onClose={handleSidebarClose}
+                    />
+                </Grid>
+                <Grid size="grow" paddingX={5}>
+                    <Outlet />
+                    <Footer />
+                </Grid>
             </Grid>
         </Grid>
     );
